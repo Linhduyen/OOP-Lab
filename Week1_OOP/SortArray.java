e54e50f369e6bccddcf73d491d6b16937a2330a6
Linhduyen <vudoanduyen123@gmail.com>
2024-03-15 21:53:02 +0700

    Complete assignment 6.4

--- Week1_OOP/SortArray.java
+++ Week1_OOP/SortArray.java
@@ -3,7 +3,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class SortArrays {
+public class SortArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of array: ");
