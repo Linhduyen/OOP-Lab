54700a7ffc805c6c35591425a2703f8079bee1fc
Linhduyen <vudoanduyen123@gmail.com>
2024-03-15 23:47:17 +0700

    Complete assignment 2.2.6

--- Week1_OOP/Equation.java
+++ Week1_OOP/Equation.java
@@ -1,112 +1,88 @@
-import java.util.Scanner;
-public class Equation {
-   public static void main(String[] args){
-    Scanner scanner = new Scanner(System.in);
-    while(TRURE){
-        System.out.println("The equation in this menu: ");
-        System.out.println("The first-degree equation (linear equation)");
-        System.out.println("The system of first-degree equations (linear system)");
-        System.out.println("The second-degree equation");
-        System.out.println("EXIT");
-        System.out.println("Enter choice");
-        int choice = sc.nextInt();
-        switch(choice){
-            case 1:
-                solvelinearsystem();
-                break;
-            case 2: 
-                solvelinearsystem();
-                break;
-            case 3: 
-                solveseconddegreeequation();
-                break;
-            case 4: 
-                sc.close();
-                System.out.println("EXIT!");
-                System.exit(0);
-            default: 
-                System.out.println("Invalid choice! Please choose again.");
-        }
-    }
-   }
-    //The first-degree equation (linear equation) with one variable
-    public static void solvelinearsystem(Scanner sc){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Solving first-degree equation: ax + b = 0");
-        System.out.print("Enter a: ");
-        int a = scanner.nextInt();
-        System.out.print("Enter b: ");
-        int b = scanner.nextInt();
-        if(a == 0){
-            if(b == 0) System.out.println("Infinity solution");
-            else System.out.println("No solution");
+import javax.swing.JOptionPane;
+public class Equation{
+
+    public static void firstsol() {
+
+        
+        String Strnum1, Strnum2; 
+        Strnum1 = JOptionPane.showInputDialog(null, "Please Input first num", "First order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
+        double num1 = Double.parseDouble(Strnum1); 
+        Strnum2 = JOptionPane.showInputDialog(null, "Please Input second number", "First order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
+        double num2 = Double.parseDouble(Strnum2); 
+        if (num1 == 0){
+            if(num2==0) JOptionPane.showMessageDialog(null,"This equation has infinite solutions!","Solution of equation",JOptionPane.INFORMATION_MESSAGE);
+            else JOptionPane.showMessageDialog(null,"This equation has no solution!","Solution of equation",JOptionPane.INFORMATION_MESSAGE);
         }
         else{
-            Double solution = -b/a;
-            System.out.println("The solution is: "+ solution);
+            double res = -num2/num1;
+            JOptionPane.showMessageDialog(null, res, "Root of first order equation", JOptionPane.INFORMATION_MESSAGE);
         }
+    }
 
-    }
-    //The system of first-degree equations (linear system) with two variables
-    public static void solvelinearsystem(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Solving linear system of equations (ax + by = c, dx + ey = f)");
-        System.out.println("Enter a: ");
-        double a = scanner.nextDouble();
-        System.out.println("Enter b: ");
-        double b = scanner.nextDouble();
-        System.out.println("Enter c: ");
-        double c = scanner.nextDouble();
-        System.out.println("Enter coefficients for the second equation (dx + ey = f):");
-        System.out.println("Enter d: ");
-        double d = scanner.nextDouble();
-        System.out.println("Enter e: ");
-        double e = scanner.nextDouble();
-        System.out.println("Enter f: ");
-        double f = scanner.nextDouble();
-        double determiant1 = a*e - b*d;
-        double determiant2 = a*f - c*d;
-        double determiant3 = b*f - a*c;
-        if(determiant1 == 0 && determiant2 == 0 && determiant3 == 0){
-            System.out.println("Infinite solution");
-        }
-        else if(determiant1 == 0){
-            System.out.println("No solution");
-        }else{
-            double x = (c*e - b*f)/determiant1;
-            double y = (a*f - c*d)/determiant1;
-            System.out.println("The solution for the system of equations is:");
-            System.out.println("x = " + x);
-            System.out.println("y = " + y);
-        }
-    }
-    public static void solveseconddegreeequation(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Solving second-degree equation (ax^2 + bx + c = 0)");
-        System.out.println("Enter a: ");
-        double a = scanner.nextDouble();
-        if(a == 0){
-            System.out.println("This is not a second-degree equation!");
-            scanner.close();
-            return;
-        }
-        System.out.print("Enter b: ");
-        double b = scanner.nextDouble();
-        System.out.print("Enter c: ");
-        double c = scanner.nextDouble();
+
+    public static void secondsol(){
         
-        double discriminant = b*b - 4*c*a;
-        if(discriminant > 0){
-            double root1 = (-b + Math.sqrt(discriminant))/ (2 *a);
-            double root2 = (-b + Math.sqrt(discriminant))/ (2 *a);
-            System.out.println("Two distinct roots: x1 = "+root1+ ", x2 = "+root2);
-        } else if( discriminant = 0){
-            double root = -b/2*a;
-            System.out.println("The equation has only one root: " + root );
-        } else {
-            System.out.println("There are no real root in this equation");
+        double a11, a12, a21, a22, b1, b2;
+    
+        a11 = Double.parseDouble((String) JOptionPane.showInputDialog(null, "Input coefficient a11:", "System of first-order equations with two variables", JOptionPane.INFORMATION_MESSAGE));
+        a12 = Double.parseDouble((String) JOptionPane.showInputDialog(null, "Input coefficient a12:", "System of first-order equations with two variables", JOptionPane.INFORMATION_MESSAGE));
+        a21 = Double.parseDouble((String) JOptionPane.showInputDialog(null, "Input coefficient a21:", "System of first-order equations with two variables", JOptionPane.INFORMATION_MESSAGE));
+        a22 = Double.parseDouble((String) JOptionPane.showInputDialog(null, "Input coefficient a22:", "System of first-order equations with two variables", JOptionPane.INFORMATION_MESSAGE));
+        b1 = Double.parseDouble((String) JOptionPane.showInputDialog(null, "Input coefficient b1:", "System of first-order equations with two variables", JOptionPane.INFORMATION_MESSAGE));
+        b2 = Double.parseDouble((String) JOptionPane.showInputDialog(null, "Input coefficient b2:", "System of first-order equations with two variables", JOptionPane.INFORMATION_MESSAGE));
+    
+        double det = a11 * a22 - a12 * a21;
+    
+        if (det == 0) {
+            if (a11 * b2 == a21 * b1 && a12 * b2 == a22 * b1) {
+                JOptionPane.showMessageDialog(null, "The system of equations has infinite solutions!", "Soltution of system of equations", JOptionPane.INFORMATION_MESSAGE);
+            } else {
+                JOptionPane.showMessageDialog(null, "The system of equations has no solutions!", "Soltution of system of equations", JOptionPane.INFORMATION_MESSAGE);
+            }
+        } 
+        else {
+            double x1 = (a22 * b1 - a12 * b2) / det;    
+            double x2 = (a11 * b2 - a21 * b1) / det;
+            JOptionPane.showMessageDialog(null, "Solution: x1 = " + x1 + ", x2 = " + x2, "Solution of system of equations", JOptionPane.INFORMATION_MESSAGE);
         }
-
+    }   
+    // second order equation with 1 variable 
+    public static void thirdsol(){
+        
+        String stra, strb, strc; 
+        stra = JOptionPane.showInputDialog(null, "Please Input a", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
+        double a = Double.parseDouble(stra);
+        strb = JOptionPane.showInputDialog(null, "Please Input b", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
+        double b = Double.parseDouble(strb);
+        strc = JOptionPane.showInputDialog(null, "Please Input c", "Second order equation with one variable", JOptionPane.INFORMATION_MESSAGE);
+        double c = Double.parseDouble(strc);
+        if(a == 0) JOptionPane.showMessageDialog(null, "Error value of a", "Error", JOptionPane.INFORMATION_MESSAGE);
+        else{
+            double delta = b*b - 4*a*c;
+            if(delta < 0) JOptionPane.showMessageDialog(null, "No solution", "Roots of equation", JOptionPane.INFORMATION_MESSAGE);
+            if(delta == 0){
+                double x = (-b - Math.sqrt(delta))/(2*a);
+                JOptionPane.showMessageDialog(null, "x1 = x2 = " + x , "Roots of equation", JOptionPane.INFORMATION_MESSAGE);
+            }
+            else{
+                double x = (-b + Math.sqrt(delta))/(2*a);
+                double y = (-b - Math.sqrt(delta))/(2*a);
+                JOptionPane.showMessageDialog(null, "x1 = " + x + ", x2 = " + y, "Roots of equation", JOptionPane.INFORMATION_MESSAGE);
+            } 
+        }
     }
-
+    public static void main(String args[]){
+        String a[]={"1","2"};
+        int numvar=   Integer.parseInt((String)JOptionPane.showInputDialog(null,"Input the number of variables:","Number of variables of equation",
+        JOptionPane.QUESTION_MESSAGE,null,a,1));
+        if(numvar == 1){
+            int degree=   Integer.parseInt((String)JOptionPane.showInputDialog(null,"Input the degree of the equation:","Degree of equation",
+            JOptionPane.QUESTION_MESSAGE,null,a,1));
+            if(degree == 1 ){
+                Equation.firstoder_onevar(); 
+            }
+            else Equation.firstoder_twovars();
+        }
+        else Equation.secondorder_onevar();
+    }
 }
