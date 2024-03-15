b4dbd11bc841c0c0dce7a9c2ec98d61647d55c25
Linhduyen <vudoanduyen123@gmail.com>
2024-03-15 20:39:49 +0700

    Complete assignment 6.3

---
+++ Week1_OOP/Triangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Triangle {
+    public static void main(String[] ags){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Height of n stars");
+        int n = scanner.nextInt();
+        for(int i = 1; i <= n ; i++){
+            for(int j = 1; j <= n - i; j++) System.out.print(" ");
+            for(int j = 1; j <= 2* i - 1; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
