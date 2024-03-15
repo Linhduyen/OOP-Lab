c962c48c5eabc9339251c18d6acb9aa02323211f
Linhduyen <vudoanduyen123@gmail.com>
2024-03-15 16:46:47 +0700

    Update

--- Week1_OOP/ChoosingOption.java
+++ Week1_OOP/ChoosingOption.java
@@ -7,14 +7,13 @@ public class ChoosingOption {
         int option = JOptionPane.showOptionDialog(null, "Do you want to change to the first class ticket?","confirmation",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE,null,options, options[0] );
         if (option == JOptionPane.YES_OPTION) {
             System.out.println("User selected 'Yes'");
-            // Do something if user selects 'Yes'
+            
         } else if (option == JOptionPane.NO_OPTION) {
             System.out.println("User selected 'No'");
-            // Do something if user selects 'No'
+           
         } else {
             System.out.println("User closed the dialog without making a  option");
-            // Do something else if user closes the dialog without making a  option
-        }
+            
     System.exit(0);
     }
 }
